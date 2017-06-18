ac15b04e86e640665933f0ea80dcba8ee200774c
戴翔宇 <daixiangyu@bytedance.com>
2017-06-18 19:17:16 +0800

    requirements

---
+++ test.vue
@@ -0,0 +1,15 @@
+<template>
+<Button type="primary"></Button>
+<Input v-model="" placeholder=""></Input>
+</template>
+
+<script>
+export default {
+    name: 'name'
+
+}
+</script>
+
+<style scoped>
+
+</style>
