cbddd216c877740b4bbbc60016297e55f25418de
戴翔宇 <daixiangyu@bytedance.com>
2017-06-18 18:32:27 +0800

    change

--- extension.js
+++ extension.js
@@ -1,30 +1,37 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 var vscode = require('vscode');
+var beautify = require('./utils/beautify');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 function activate(context) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "ivue" is now active!');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    var disposable = vscode.commands.registerCommand('extension.sayHello', function () {
-        // The code you place here will be executed every time your command is executed
+    var formatter = vscode.commands.registerTextEditorCommand('ivue.format', function (textEditor) {
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        if (textEditor.document.languageId !== 'vue' || !/\.vue$/i.test(textEditor.document.fileName)) {
+            return;
+        }
+
+        var editorInsertSpace = textEditor.options.insertSpaces;
+        var editorTabSize = textEditor.options.tabSize;
+
+        var text = textEditor.document.getText();
+
+        var code = beautify(text, !editorInsertSpace, editorTabSize);
+
+        textEditor.edit(function (editBuilder) {
+            var document = textEditor.document;
+            var lastLine = document.lineAt(document.lineCount - 1);
+            var start = new vscode.Position(0, 0);
+            var end = new vscode.Position(document.lineCount - 1, lastLine.text.length);
+            var range = new vscode.Range(start, end);
+            editBuilder.replace(range, code);
+        });
     });
 
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(formatter);
 }
 exports.activate = activate;
 
 // this method is called when your extension is deactivated
-function deactivate() {
-}
+function deactivate() {}
 exports.deactivate = deactivate;
